9e041c3423244db56b7dc6d2e0f58f00117d6174
Gabor Retvari <retvari@tmit.bme.hu>
2023-01-11 22:50:53 +0100

    fix: add RBAC ruples to permit setting gatewayconfigs/finalizer

--- internal/controllers/rbac.go
+++ internal/controllers/rbac.go
@@ -4,6 +4,7 @@ package controllers
 // +kubebuilder:rbac:groups="gateway.networking.k8s.io",resources=gatewayclasses;gateways;udproutes,verbs=get;list;watch;update;patch
 // +kubebuilder:rbac:groups="gateway.networking.k8s.io",resources=gatewayclasses/status;gateways/status;udproutes/status,verbs=update;patch
 // +kubebuilder:rbac:groups="stunner.l7mp.io",resources=gatewayconfigs,verbs=get;list;watch;update;patch
+// +kubebuilder:rbac:groups="stunner.l7mp.io",resources=gatewayconfigs/finalizers,verbs=update
 
 // RBAC for references in watched resources.
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
